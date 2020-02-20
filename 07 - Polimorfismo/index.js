7b942c8ed9a3b80ade352462b8a617ffd0ba39cf
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2020-02-20 19:31:57 -0300

    mini_curso_JS_POO

--- 07 - Polimorfismo/index.js
+++ 07 - Polimorfismo/index.js
@@ -1,74 +1,36 @@
-// define a classe Animal
-function Animal() {}
-
-//Um protótipo nos permite predefinir propriedades, incluindo métodos
-Animal.prototype.andar = function(){
-  console.log ('Estou andando!');
-};
-Animal.prototype.dormir = function(){
-  console.log ('Estou dormindo!');
-};
-
-///////////////////////////////////////////////////////////////////////////////////
-
-function Cachorro() {
-  // Chama o método parente
-  Animal.call(this);
-}
- 
-// herda de Animal
-Cachorro.prototype = new Animal();
-
-// corrigir o ponteiro construtor porque aponta para Animal
-Cachorro.prototype.constructor = Cachorro;
-
-// adiciona o método dizOi
-Cachorro.prototype.andar = function(){
-  console.log('Oi, eu sou Cachorro e estou andando');
+class Animal {
+    
+    constructor(nome) { this._nome = nome; }
+    mover(distancia) {
+        console.log(`${this._nome} se moveu ${distancia} metros.`);
+    }
 }
 
-// adiciona o método dizTchau 
-Cachorro.prototype.latir = function(){
-  console.log('Au au');
+const animal = new Animal("Cachorro");
+animal.mover(50);
+
+animal._nome = "Gato";
+animal.mover(50);
+
+
+class Cobra extends Animal {
+    constructor(_nome) { super(_nome); }
+    mover(distancia = 5) {
+        console.log("Rastejando... ");
+        super.mover(distancia);
+    }
 }
 
-var animal = new Animal();
-animal.andar();
-animal.dormir();
+const cobra = new Cobra ("Serpente");
+cobra.mover(15);
 
-var cachorro = new Cachorro();
-cachorro.andar();
-cachorro.dormir();
-cachorro.latir();
-
-/////////////////////////////////////////////////////////////////////////////
-
-function Gato() {
-  // Chama o método parente
-  Animal.call(this);
-}
- 
-// herda de Animal
-Gato.prototype = new Animal();
-
-// corrigir o ponteiro construtor porque aponta para Animal
-Gato.prototype.constructor = Gato;
-
-// adiciona o método dizOi
-Gato.prototype.andar = function(){
-  console.log('Oi, eu sou Gato e estou andando');
+class Cavalo extends Animal {
+    constructor(_nome) { super(_nome); }
+    mover(distancia = 45) {
+        console.log("Galopando...");
+        super.mover(distancia);
+    }
 }
 
-// adiciona o método dizTchau 
-Gato.prototype.miar = function(){
-  console.log('Miau');
-}
-
-var animal = new Animal();
-animal.andar();
-animal.dormir();
-
-var gato = new Gato();
-gato.andar();
-gato.dormir();
-gato.miar();
+const cavalo = new Cavalo ("Corredor");
+cavalo.mover(60);
