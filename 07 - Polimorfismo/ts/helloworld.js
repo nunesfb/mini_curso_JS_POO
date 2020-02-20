f45f24da3ec6589ccd2f22b458871a50e68f0d32
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2020-02-20 15:06:17 -0300

    remove

--- 07 - Polimorfismo/ts/helloworld.js
+++
@@ -1,5 +0,0 @@
-//variável mensagem com tipo definido (string)
-var mensagem = "Hello World";
-//console.log recebendo como parâmetro a variável 'mensagem', 
-//para exibir o texto associado a ela no console do navegador.
-console.log(mensagem);
